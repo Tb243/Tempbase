c7c187c820a1d942b124d05cf4ac2ed5a56e0b26
Richard Denton <isdampe@gmail.com>
2021-09-02 09:24:36 +1000

    NEW: Add physical hcsr04

---
+++ hal/devices/hcsp04.py
@@ -0,0 +1,35 @@
+from devices.device import Device
+import RPi.GPIO as GPIO
+import time
+
+class HCSP04(Device):
+
+	def __init__(self, echoPin=None, triggerPin=None):
+		self.name = "HCSP04"
+		self.isVirtual = False
+		self.echoPin = echoPin
+		self.triggerPin = triggerPin
+
+	def setup(self):
+		GPIO.setup(self.echoPin, GPIO.IN)
+		GPIO.setup(self.triggerPin, GPIO.OUT)
+		GPIO.output(self.triggerPin, 0)
+		time.sleep(2.0)
+
+	def destroy(self):
+		GPIO.cleanup()
+
+	def read(self):
+		GPIO.output(self.triggerPin, 1)
+		time.sleep(0.00001)
+		GPIO.output(self.triggerPin, 0)
+
+		pulseStart = time.time()
+		while GPIO.input(self.echoPin) == 0:
+			pulseStart = time.time()
+
+		pulseEnd = time.time()
+		while GPIO.input(self.echoPin) == 1:
+			pulseEnd = time.time()
+
+		return (pulseEnd - pulseStart) * 17150
