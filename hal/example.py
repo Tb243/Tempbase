f892ee1e68237149921e9a73c664fc814b20ada0
Richard Denton <isdampe@gmail.com>
2021-09-04 14:52:58 +1000

    UPDATE: Change example usage file to include MLX90614

--- hal/example.py
+++ hal/example.py
@@ -1,4 +1,5 @@
 from devices import virtual_hcsr04
+from devices import virtual_mlx90614
 
 device = virtual_hcsr04.VirtualHCSR04()
 print("Setting up device...")
@@ -7,3 +8,7 @@ print("Reading distance...")
 distance = round(device.read(), 2)
 print("The distance is approximately %sCM" % distance)
 device.destroy()
+
+temperature_device = virtual_mlx90614.VirtualMLX90614()
+temperature_device.setup()
+print("The temperature is %s" % temperature_device.read())
