5ae9d6be710eebd120dc7f2a5d106419e639ded6
Jesse Hancock <jesse.hancock96@hotmail.com>
2021-09-07 16:08:57 +1000

    NEW Add Buzzer device file T2.1.1

---
+++ hal/devices/virtual_buzzer5v.py
@@ -0,0 +1,25 @@
+from devices.device import Device
+import RPi.GPIO as GPIO
+import time
+
+class VirtualBUZZER5V(Device):
+    
+	def __init__(self, buzzerPin=None):
+		self.name = "Virtual BUZZER5V"
+		self.isVirtual = True
+		self.buzzerPin = buzzerPin
+
+	def validateConfig(self):
+		return True
+
+	def setup(self):
+		time.sleep(2.0)
+
+	def destroy(self):
+		pass
+
+	def read(self):
+		pass
+
+	def buzz(self):
+		time.sleep(1)
