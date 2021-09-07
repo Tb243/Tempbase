9a1e32658dd273f605c73b34363ba3e7d0f765aa
Jesse Hancock <jesse.hancock96@hotmail.com>
2021-09-07 16:10:01 +1000

    UPDATE Add buzzer device unit to test_devices

--- hal/test_devices.py
+++ hal/test_devices.py
@@ -2,83 +2,102 @@ import unittest
 import sys
 from devices import virtual_hcsr04
 from devices import virtual_mlx90614
+from devices import virtual_buzzer5v
 
 # Physical devices should each be wrapped in try except blocks
 try:
-	from devices import hcsr04
-	from devices import mlx90614
+    from devices import hcsr04
+    from devices import mlx90614
+    from devices import buzzer5v
 except:
-	pass
+    pass
 
 HCSR04_ECHO_PIN = 13
 HCSR04_TRIGGER_PIN = 6
+BUZZER5V_BUZZER_PIN = 26
 
 class DeviceTests:
-	def testSetup(self):
-		try:
-			self.device.setup()
-		except:
-			self.fail("setup() has not been implemented for device %s" % self.device.name)
+    def testSetup(self):
+        try:
+            self.device.setup()
+        except:
+            self.fail("setup() has not been implemented for device %s" % self.device.name)
 
-	def testValidateConfig(self):
-		try:
-			self.assertTrue(self.device.validateConfig())
-		except:
-			self.fail("validateConfig() has not been implemented for device %s" % self.device.name)
+    def testValidateConfig(self):
+        try:
+            self.assertTrue(self.device.validateConfig())
+        except:
+            self.fail("validateConfig() has not been implemented for device %s" % self.device.name)
 
-	def testRead(self):
-		try:
-			self.device.read()
-		except:
-			self.fail("read() has not been implemented for device %s" % self.device.name)
-
+    def testRead(self):
+        try:
+            self.device.read()
+        except:
+            self.fail("read() has not been implemented for device %s" % self.device.name)
 
 # This needs to be wrapped to prevent tests from crashing on
 # devices that don't have RPi.GPIO installed.
 if "hcsr04" in sys.modules:
-	class TestPhysicalHCSR04(unittest.TestCase, DeviceTests):
-		def setUp(self):
-			self.device = hcsr04.HCSR04(HCSR04_ECHO_PIN, HCSR04_TRIGGER_PIN)
-			try:
-				self.device.setup()
-			except:
-				pass
+    class TestPhysicalHCSR04(unittest.TestCase, DeviceTests):
+        def setUp(self):
+            self.device = hcsr04.HCSR04(HCSR04_ECHO_PIN, HCSR04_TRIGGER_PIN)
+            try:
+                self.device.setup()
+            except:
+                pass
 
 if "mlx90614" in sys.modules:
-	class TestPhysicalMLX90614(unittest.TestCase, DeviceTests):
-		def setUp(self):
-			self.device = mlx90614.MLX90614()
-			try:
-				self.device.setup()
-			except:
-				pass
+    class TestPhysicalMLX90614(unittest.TestCase, DeviceTests):
+        def setUp(self):
+            self.device = mlx90614.MLX90614()
+            try:
+                self.device.setup()
+            except:
+                pass
 
+if "buzzer5v" in sys.modules:
+    class TestPhysicalBUZZER5V(unittest.TestCase, DeviceTests):
+        def setUp(self):
+            self.device = buzzer5v.BUZZER5V()
+            try:
+                self.device.setup()
+            except:
+                pass
+            
 class TestVirtualHCSR04(unittest.TestCase, DeviceTests):
 
-	def setUp(self):
-		self.device = virtual_hcsr04.VirtualHCSR04(HCSR04_ECHO_PIN, HCSR04_TRIGGER_PIN)
+    def setUp(self):
+        self.device = virtual_hcsr04.VirtualHCSR04(HCSR04_ECHO_PIN, HCSR04_TRIGGER_PIN)
 
-	def testRead(self):
-		try:
-			value = self.device.read()
-			self.assertTrue(value >= 0.5 and value <= 15.0)
-		except:
-			self.fail("read() has not been implemented for device %s" % self.device.name)
+    def testRead(self):
+        try:
+            value = self.device.read()
+            self.assertTrue(value >= 0.5 and value <= 15.0)
+        except:
+            self.fail("read() has not been implemented for device %s" % self.device.name)
 
 class TestVirtualMLX90614(unittest.TestCase, DeviceTests):
 
-	def setUp(self):
-		self.device = virtual_mlx90614.VirtualMLX90614()
+    def setUp(self):
+        self.device = virtual_mlx90614.VirtualMLX90614()
 
-	def testRead(self):
-		try:
-			value = self.device.read()
-			self.assertTrue(value >= -20 and value <= 100)
-		except:
-			self.fail("read() has not been implemented for device %s" % self.device.name)
+    def testRead(self):
+        try:
+            value = self.device.read()
+            self.assertTrue(value >= -20 and value <= 100)
+        except:
+            self.fail("read() has not been implemented for device %s" % self.device.name)
 
+class TestVirtualBuzzer5v(unittest.TestCase, DeviceTests):
+    
+    def setUp(self):
+        self.device = virtual_buzzer5v.VirtualBUZZER5V()
 
-
+    def testBuzz(self):
+        try:
+            value = self.device.buzz()
+        except:
+            self.fail("read() has not been implemented for device %s" % self.device.name)
 
 if __name__ == "__main__":
-	unittest.main(verbosity=2)
+    unittest.main(verbosity=2)
