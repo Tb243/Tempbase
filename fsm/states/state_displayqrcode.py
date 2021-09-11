f84b565ea30ff1cea682c30a182ba335bf09ef6f
ginge270 <63086783+ginge270@users.noreply.github.com>
2021-09-11 14:57:04 +1000

    NEW: Add state file 'Display QR Code" state T5.1.1

---
+++ fsm/states/state_displayqrcode.py
@@ -0,0 +1,24 @@
+from states import state
+import time
+
+class StateDisplayQrCode(state.FsmState):
+
+	def __init__(self, fsm):
+		self.identifier = "displayQrCode"
+		self.label = "Display QR Code"
+		self.fsm = fsm
+
+	def onEnterState(self, args=None):
+		print("Entering the display QR code state")
+		
+
+	def onExitState(self):
+		print("Leaving the display QR code state")
+
+	def main(self):
+		#display qr code
+		print("QR Code being displayed")
+		#wait 10 seconds
+		print("waiting 10 seconds")
+		time.sleep(0.10)
+		self.fsm.transitionState("waitForHand")
