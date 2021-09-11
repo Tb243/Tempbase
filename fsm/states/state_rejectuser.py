34dcb4313f0ecbf8e277ba8dc825beb20b275f45
ginge270 <63086783+ginge270@users.noreply.github.com>
2021-09-11 14:56:36 +1000

    NEW: Add state file 'Reject User State' T3.1.1

---
+++ fsm/states/state_rejectuser.py
@@ -0,0 +1,27 @@
+from states import state
+import time
+
+class StateRejectUser(state.FsmState):
+
+	def __init__(self, fsm):
+		self.identifier = "rejectUser"
+		self.label = "Reject User"
+		self.fsm = fsm
+
+	def onEnterState(self, args=None):
+		print("Entering the reject user state")
+		
+
+	def onExitState(self):
+		print("Leaving the reject user state")
+
+	def main(self):
+		#display red cross
+		print("Displaying red cross")
+		time.sleep(0.05)
+		#if temperature has already been read then
+		print("Please measure your temperature again")
+		self.fsm.transitionState("measureTemperature")
+		#else
+		#display message saying to get a COVID test
+		#self.fsm.transitionState("waitForHand")
