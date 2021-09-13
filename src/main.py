7deb27d5abed2ef5434265beb3a85d961995aff2
Richard Denton <isdampe@gmail.com>
2021-09-13 15:14:40 +1000

    ENHANCE: Add logging and virtual globals

--- src/main.py
+++ src/main.py
@@ -7,12 +7,17 @@ from fsm import state_displayqrcode
 from fsm import state_rejectuser
 
 VIRTUAL_MODE = True
+DEBUG_ON = True
 
 class Fsm:
 
     def __init__(self):
         self.states = {}
         self.currentState = None
+
+    def log(self, message):
+        if DEBUG_ON:
+            print("FSM: " + message)
 
     def addState(self, state):
         self.states[state.identifier] = state
@@ -22,9 +27,12 @@ class Fsm:
             raise("Tried to transition to state '%s' which is an unknown state identifier" % nextState)
 
         if self.currentState and self.currentState in self.states:
+            self.log("Calling onExitState for state '%s'" % self.currentState)
             self.states[self.currentState].onExitState()
 
+        self.log("Setting state to '%s'" % nextState)
         self.currentState = nextState
+        self.log("Calling onEnterState for state '%s'" % self.currentState)
         self.states[self.currentState].onEnterState(args)
 
     def spin(self):
@@ -34,6 +42,7 @@ class Fsm:
         if not self.currentState in self.states:
             raise("Tried to spin in state '%s' which is an unknown state identifier" % self.currentState)
 
+        self.log("Calling main for state '%s" % self.currentState)
         self.states[self.currentState].main()
         self.spin()
 
@@ -41,26 +50,23 @@ if __name__ == "__main__":
     fsm = Fsm()
     fsm.addState(state_setup.StateSetup(fsm))
     fsm.addState(state_waitforhand.StateWaitForHand(fsm))
-    fsm.transitionState("setup")
 
     fsm.addState(state_setup.StateSetup(fsm))
     fsm.addState(state_dispensesanitiser.StateDispenseSanitiser(fsm))
-    fsm.transitionState("setup")
 
     fsm.addState(state_setup.StateSetup(fsm))
     fsm.addState(state_measuretemperature.StateMeasureTemperature(fsm))
-    fsm.transitionState("setup")
     
     fsm.addState(state_setup.StateSetup(fsm))
     fsm.addState(state_processandstoretemp.StateProcessAndStoreTemp(fsm))
-    fsm.transitionState("setup")
  
     fsm.addState(state_setup.StateSetup(fsm))
     fsm.addState(state_displayqrcode.StateDisplayQrCode(fsm))
-    fsm.transitionState("setup")
     
     fsm.addState(state_setup.StateSetup(fsm))
     fsm.addState(state_rejectuser.StateRejectUser(fsm))
+
+    # Set initial state
     fsm.transitionState("setup")
     
     fsm.spin()
