fde070580f68f606c99e52f96d3a07c92989aa0f
Richard Denton <isdampe@gmail.com>
2021-09-13 17:37:12 +1000

    ENHANCE: Implement HAL devices for dispense state machine

--- src/fsm/state_dispensesanitiser.py
+++ src/fsm/state_dispensesanitiser.py
@@ -1,5 +1,14 @@
+import os
 from fsm.state import FsmState
-from hal import virtual_hcsr04
+
+VIRTUAL_MODE = True if os.environ.get("virtualMode") == "on" else False
+
+if VIRTUAL_MODE:
+    from hal.virtual_fs90r import VirtualFS90R as FS90R
+    from hal.virtual_sen0368 import VirtualSEN0368 as SEN0368
+else:
+    from hal.fs90r import FS90R
+    from hal.sen0368 import SEN0368
 
 class StateDispenseSanitiser(FsmState):
 
@@ -7,21 +16,24 @@ class StateDispenseSanitiser(FsmState):
         self.identifier = "dispenseSanitiser"
         self.label = "Dispense Sanitiser"
         self.fsm = fsm
+        self.servoMotor = FS90R(12)
+        self.liquidSensor = SEN0368(20)
 
     def onEnterState(self, counter):
         # src04.setup()
         self.counter = counter
+        self.servoMotor.setup()
+        self.liquidSensor.setup()
 
     def onExitState(self):
-        pass
-        # Check if sanitiser level is empty
-        # If it is, send an alert to the device owner.
+        # If the liquid level is low, send alert to device owner
+        if self.liquidSensor.read() == 0:
+            self.sendAlert()
 
     def main(self):
-        while True:
-            # turn = fs90r.turn()
-            turn = float(input("Enter 0 for sanitiser to dispense: "))
-            if turn < 1:
-                break
-
+        self.servoMotor.turn()
         self.fsm.transitionState("measureTemperature", self.counter)
+
+    def sendAlert(self):
+        # Send the alert to the device owner
+        pass
