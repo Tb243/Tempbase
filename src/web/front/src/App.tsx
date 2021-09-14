9f8e11a0e8cf0808be90240df76668c932a46853
Richard Denton <isdampe@gmail.com>
2021-09-14 21:46:44 +1000

    FIX: Add basic state

--- src/web/front/src/App.tsx
+++ src/web/front/src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type TProps = {};
+type TState = {
+	screen: "setup" | "waitForHand" | "rejectUser" | "processAndStoreTemperature" | "measureTemperature" | "displayQrCode" | "dispenseSanitiser";
+};
+
+class App extends React.Component<TProps, TState> {
+
+	constructor(props: TProps) {
+		super(props);
+
+		this.state = {
+			screen: "setup"
+		};
+	}
+
+	componentDidMount() {
+
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<p>
+						The current FSM state is <code>{this.state.screen}</code>
+					</p>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
