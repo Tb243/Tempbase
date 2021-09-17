7f33816f362c274dea087ab71d7fc707914815e5
Richard Denton <isdampe@gmail.com>
2021-09-18 09:46:42 +1000

    TEST: Add event tests.

--- src/database/test_database.py
+++ src/database/test_database.py
@@ -1,4 +1,5 @@
 from model import settingsModel
+from model import temperatureEventModel
 
 settings = settingsModel.SettingsModel(True)
 value = settings.get("version")
@@ -7,3 +8,9 @@ if value == None:
 	settings.set("version", "1.0")
 else:
 	print(value)
+
+event = temperatureEventModel.TemperatureEventModel(True)
+event.recordTemperature(36, True)
+event.recordTemperature(40, False)
+
+print(event.getMostRecentTemperatures())
