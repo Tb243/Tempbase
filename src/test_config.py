5425899457ddb312a5b2678f3120f9882a8af83b
Richard Denton <isdampe@gmail.com>
2021-09-18 09:09:08 +1000

    CONFIG: Add pin configurations and test for hal

--- src/test_config.py
+++ src/test_config.py
@@ -21,6 +21,31 @@ class DeviceTests(unittest.TestCase):
 		self.assertTrue("recipients" in config["email"])
 		self.assertTrue(isinstance(config["email"]["recipients"], list))
 
+	def test_hal(self):
+		self.assertTrue("hal" in config)
+
+	def test_hcsr04(self):
+		self.assertTrue("HCSR04" in config["hal"])
+		self.assertTrue("echoPin" in config["hal"]["HCSR04"])
+		self.assertTrue(isinstance(config["hal"]["HCSR04"]["echoPin"], int))
+		self.assertTrue("triggerPin" in config["hal"]["HCSR04"])
+		self.assertTrue(isinstance(config["hal"]["HCSR04"]["triggerPin"], int))
+
+	def test_buzzer(self):
+		self.assertTrue("BUZZER" in config["hal"])
+		self.assertTrue("pin" in config["hal"]["BUZZER"])
+		self.assertTrue(isinstance(config["hal"]["BUZZER"]["pin"], int))
+
+	def test_fs90r(self):
+		self.assertTrue("FS90R" in config["hal"])
+		self.assertTrue("pin" in config["hal"]["FS90R"])
+		self.assertTrue(isinstance(config["hal"]["FS90R"]["pin"], int))
+
+	def test_sen0368(self):
+		self.assertTrue("SEN0368" in config["hal"])
+		self.assertTrue("pin" in config["hal"]["SEN0368"])
+		self.assertTrue(isinstance(config["hal"]["SEN0368"]["pin"], int))
+
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
