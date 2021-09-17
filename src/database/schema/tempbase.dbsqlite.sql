a1ed37cf797cbbaea580726c2c19355c387f7770
Richard Denton <isdampe@gmail.com>
2021-09-18 09:47:10 +1000

    REFACTOR: Simplify temperatureEvent table

--- src/database/schema/tempbase.dbsqlite.sql
+++ src/database/schema/tempbase.dbsqlite.sql
@@ -9,20 +9,23 @@ BEGIN TRANSACTION;
 -- Table: settings
 CREATE TABLE settings (settingName VARCHAR(64) PRIMARY KEY NOT NULL UNIQUE, settingValue TEXT NOT NULL, timeUpdated DATETIME NOT NULL);
 
--- Table: temperature_event
-CREATE TABLE temperature_event (userIdentifier VARCHAR(128), temperatureReadingCelcius DOUBLE NOT NULL, distanceFromSensorCm DOUBLE NOT NULL, outcome INTEGER NOT NULL, timeCreated DATETIME NOT NULL);
+CREATE TABLE temperature_event (
+    temperatureReadingCelcius DOUBLE   NOT NULL,
+    outcome                   INTEGER  NOT NULL,
+    timeCreated               DATETIME NOT NULL
+);
 
--- Index: idx_temperature_event_outcome
-CREATE INDEX idx_temperature_event_outcome ON temperature_event (outcome);
+CREATE INDEX idx_temperature_event_outcome ON temperature_event (
+    outcome
+);
 
--- Index: idx_temperature_event_reading
-CREATE INDEX idx_temperature_event_reading ON temperature_event (temperatureReadingCelcius);
+CREATE INDEX idx_temperature_event_reading ON temperature_event (
+    temperatureReadingCelcius
+);
 
--- Index: idx_temperature_event_sensor
-CREATE INDEX idx_temperature_event_sensor ON temperature_event (distanceFromSensorCm);
-
--- Index: idx_temperature_event_time_created
-CREATE INDEX idx_temperature_event_time_created ON temperature_event (timeCreated);
+CREATE INDEX idx_temperature_event_time_created ON temperature_event (
+    timeCreated
+);
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
