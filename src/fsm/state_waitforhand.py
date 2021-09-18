347ac1e77bef53f20b7d05ab56a61466dc5edf97
Richard Denton <isdampe@gmail.com>
2021-09-18 17:14:31 +1000

    FIX: Go to successful reads

--- src/fsm/state_waitforhand.py
+++ src/fsm/state_waitforhand.py
@@ -24,13 +24,18 @@ class StateWaitForHand(FsmState):
 
     def main(self):
         counter = 0
+        successfulReads = 0
         while True:
             distance = self.hcsr04.read()
             self.fsm.setStateData("ultrasonicDistance", distance)
             self.log("Distance to hand: %f" % distance)
             if distance < 10:
+                successfulReads += 1
+
+            if successfulReads >= 7:
                 break
-            time.sleep(2)
+
+            time.sleep(0.1)
 
 
         self.fsm.transitionState("dispenseSanitiser", counter)
