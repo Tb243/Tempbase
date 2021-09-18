c5f6a64b49ea429d60f96cbb2cf9f8df5731edda
Richard Denton <isdampe@gmail.com>
2021-09-18 16:51:35 +1000

    Debug

--- src/hal/hcsr04.py
+++ src/hal/hcsr04.py
@@ -20,15 +20,21 @@ class HCSR04(Device):
 		GPIO.cleanup()
 
 	def read(self):
+		print("Start read")
+		print("Pins are trigger=%d and echo=%d" % (self.triggerPin, self.echoPin))
+		print("Set high trigger pin")
 		GPIO.output(self.triggerPin, 1)
 		time.sleep(0.00001)
+		print("Set low trigger pin")
 		GPIO.output(self.triggerPin, 0)
 
 		pulseStart = time.time()
+		print("Pulse start")
 		while GPIO.input(self.echoPin) == 0:
 			pulseStart = time.time()
 
 		pulseEnd = time.time()
+		print("Pulse end")
 		while GPIO.input(self.echoPin) == 1:
 			pulseEnd = time.time()
 
