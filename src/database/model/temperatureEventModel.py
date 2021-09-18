1851aba8f324f092139fcfe6d0a42a85b147f418
Richard Denton <isdampe@gmail.com>
2021-09-18 11:45:49 +1000

    ENHANCE: Add getCount of successful entries.

--- src/database/model/temperatureEventModel.py
+++ src/database/model/temperatureEventModel.py
@@ -7,5 +7,12 @@ class TemperatureEventModel(Model):
 		self.query("INSERT INTO temperature_event(temperatureReadingCelcius, outcome, timeCreated) VALUES(?, ?, ?)",
 			[temperature, 1 if wasPermittedEntry == True else 0, timeStamp])
 
+	def countSuccessfulEntries(self):
+		result = self.query("SELECT COUNT(*) AS c FROM temperature_event WHERE outcome = 1", [], True)
+		if len(result) > 0:
+			return int(result[0]["c"])
+
+		return 0
+
 	def getMostRecentTemperatures(self, max=100):
 		return self.query("SELECT * FROM temperature_event ORDER BY timeCreated DESC LIMIT 0, ?", [max], True)
