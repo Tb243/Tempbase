8a161227574d3c4839f0c078dc4c0eb2ce6fb7ca
ginge270 <63086783+ginge270@users.noreply.github.com>
2021-09-20 18:19:52 +1000

    UPDATE: Second reject user screen T8.4.2

--- src/web/front/src/screens/RejectUser.tsx
+++ src/web/front/src/screens/RejectUser.tsx
@@ -8,7 +8,7 @@ export default class RejectUserScreen extends React.Component<TProps> {
 
 	renderFirstScreen() {
 		return (
-			<div className="screenRejectUser">
+			<div className="screenRejectUser1">
 				<p>Warning: elevated temperature!</p>
 				<p className='p1'>Your temperature is {this.props.data.temperatureMeasurement?.toFixed(1)}</p>
 				<img src="cross.png" alt="black x image"></img>
@@ -19,11 +19,11 @@ export default class RejectUserScreen extends React.Component<TProps> {
 
 	renderSecondScreen() {
 		return (
-			<div className="screenRejectUser">
-				<p>Warning: elevated temperature (second screen)!</p>
+			<div className="screenRejectUser2">
+				<p>Warning: Temperature is too high!</p>
 				<p className='p1'>Your temperature is {this.props.data.temperatureMeasurement?.toFixed(1)}</p>
+				<p className='p2'> Please take a COVID-19 test and isolate.</p>
 				<img src="cross.png" alt="black x image"></img>
-				<p className='p2'>Please measure your temperature again...</p>
 			</div>
 		);
 	}
