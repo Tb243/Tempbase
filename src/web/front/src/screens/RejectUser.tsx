26f241eb1e7d01f2b5dafe7f65da505a412ee43a
Richard Denton <isdampe@gmail.com>
2021-09-20 17:00:05 +1000

    ENHANCE: Add basic front-end logic for second screen iteration

--- src/web/front/src/screens/RejectUser.tsx
+++ src/web/front/src/screens/RejectUser.tsx
@@ -5,7 +5,8 @@ type TProps = {
 };
 
 export default class RejectUserScreen extends React.Component<TProps> {
-	render() {
+
+	renderFirstScreen() {
 		return (
 			<div className="screenRejectUser">
 				<p>Warning: elevated temperature!</p>
@@ -14,5 +15,23 @@ export default class RejectUserScreen extends React.Component<TProps> {
 				<p className='p2'>Please measure your temperature again...</p>
 			</div>
 		);
+	}
+
+	renderSecondScreen() {
+		return (
+			<div className="screenRejectUser">
+				<p>Warning: elevated temperature (second screen)!</p>
+				<p className='p1'>Your temperature is {this.props.data.temperatureMeasurement?.toFixed(1)}</p>
+				<img src="cross.png" alt="black x image"></img>
+				<p className='p2'>Please measure your temperature again...</p>
+			</div>
+		);
+	}
+
+	render() {
+		if (this.props.data.attemptCounter! < 2)
+			return this.renderFirstScreen()
+		else
+			return this.renderSecondScreen()
 	}
 }
