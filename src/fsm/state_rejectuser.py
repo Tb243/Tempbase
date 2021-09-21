d6c74c75a0a51db58fb27f38a8781adae563f08b
Richard Denton <isdampe@gmail.com>
2021-09-22 09:42:58 +1000

    REFACTOR: Optimize state process for rejecting user

--- src/fsm/state_rejectuser.py
+++ src/fsm/state_rejectuser.py
@@ -8,63 +8,57 @@ from config import config
 VIRTUAL_MODE = True if os.environ.get("virtualMode") == "on" else False
 
 if VIRTUAL_MODE:
-    from hal.virtual_buzzer5v import VirtualBUZZER5V as Buzzer5v
+	from hal.virtual_buzzer5v import VirtualBUZZER5V as Buzzer5v
 else:
-    from hal.buzzer5v import Buzzer5v
+	from hal.buzzer5v import Buzzer5v
 
 class StateRejectUser(FsmState):
 
-    def __init__(self, fsm):
-        self.identifier = "rejectUser"
-        self.label = "Reject User"
-        self.fsm = fsm
-        self.buzzer = Buzzer5v(26)
-        
-    def onEnterState(self, args):
-        self.temperature = args[0]
-        self.counter = args[1]
-        self.buzzer.setup()
+	def __init__(self, fsm):
+		self.identifier = "rejectUser"
+		self.label = "Reject User"
+		self.fsm = fsm
+		self.buzzer = Buzzer5v(26)
+		self.smsNotifier = SMSNotify(config["twilio"]["systemNumber"], config["twilio"]["accountSID"], config["twilio"]["authTok"])
+		self.emailNotifier = Notifications(config["email"]["transport"]["username"], config["email"]["transport"]["password"])
+		
+	def onEnterState(self, args):
+		self.temperature = args[0]
+		self.counter = args[1]
+		self.buzzer.setup()
 
-    def onExitState(self):
-        pass
+	def onExitState(self):
+		pass
 
-    def main(self):
-        #display red cross
-        self.log("Displaying red cross")
-        time.sleep(5)
-        if self.counter == 1:
-            self.buzzer.buzz(0.5)
-            self.log("BUZZ")
-            self.log("Please measure your temperature again")
-            time.sleep(2)
-            self.fsm.transitionState("measureTemperature", self.counter)
-        else:
-            self.log("Temperature is too high please take a COVID test and isolate")
-            self.buzzer.buzz(0.5)
-            self.log("BUZZ")
-            time.sleep(2)
-            self.sendAlert(self.temperature, 1, 1)
-            self.fsm.transitionState("waitForHand")
-            
-        # Send the alert to the device owner - 1 for yes, 0 for no
-    def sendAlert(self, temp, sms, email):
-        self.temperature = temp
-        smsAlert = sms
-        emailAlert = email
+	def main(self):
+		self.log("Displaying red cross")
+		self.buzzer.buzz(0.5)
 
-        try:
-            if smsAlert == 1:
-                for recipient in config["twilio"]["numberTo"]:
-                    SMSNotify.smsTemp(recipient, self.temperature)
-                self.log("Santise SMS success")
-            else:
-                pass
-            
-            if emailAlert == 1:
-                for recipient in config["email"]["recipients"]:
-                    Notifications.sendTemp(recipient, self.temperature)
-                self.log("Sanitise email success")
-            else:
-                pass
-        except:
-            self.log("Unsuccesful")
+		if self.counter == 1:
+			self.log("Please measure your temperature again")
+			time.sleep(7)
+			self.fsm.transitionState("measureTemperature", self.counter)
+		else:
+			self.log("Temperature is too high please take a COVID test and isolate")
+			self.sendAlert()
+			time.sleep(7)
+			self.fsm.transitionState("waitForHand")
+			
+	def sendAlert(self):
+		if len(config["twilio"]["numberTo"]) > 0:
+			for recipient in config["twilio"]["numberTo"]:
+				try:
+					self.log("SMS notify " + recipient)
+					self.smsNotifier.smsTemp(recipient, round(self.temperature, 1))
+					self.log("Santise SMS sent")
+				except:
+					self.log("Error sending SMS notification")
+		
+		if len(config["email"]["recipients"]) > 0:
+			for recipient in config["email"]["recipients"]:
+				try:
+					self.log("Email notify " + recipient)
+					self.emailNotifier.sendTemp(recipient, round(self.temperature, 1))
+					self.log("Sanitise email sent")
+				except:
+					self.log("Error sending email notifications")
