293a5f04780f35c1f21e479f40514062cc88b1a8
Richard Denton <isdampe@gmail.com>
2021-09-22 09:42:32 +1000

    REFACTOR: Fix couple of issues on NotifySMS class.

--- src/notifications/notify_sms.py
+++ src/notifications/notify_sms.py
@@ -5,19 +5,13 @@ from config import config
 class SMSNotify:
     
     def __init__(self, system_number, account_sid, auth_token):
-        self.name = "SMSNOTIFICATIONS"
         self.system_number = system_number
         self.account_sid = account_sid
         self.auth_token = auth_token
-    
-    
-    def setup(self):
-        pass
+        self.client = Client(self.account_sid, self.auth_token)
     
     def sendSMS(self, to, body):
-        client = Client(self.account_sid, self.auth_token)
-
-        message = client.messages \
+        self.client.messages \
                         .create(
                             body=body,
                             from_=self.system_number,
@@ -25,47 +19,13 @@ class SMSNotify:
                         )
     
     def smsTemp(self, recipient, temperature):
-        numberTo = recipient
-        numberFrom = config["twilio"]["numberFrom"]
-        #numberTo = config["twilio"]["numberTo"]
-        notifier = SMSNotify(numberFrom, config["twilio"]["accountSID"], config["twilio"]["authTok"])
-        
-        smsBody = "TempBase Alert: elevated temperature of " + str(temperature) + "detected."
-       
-        try:
-           notifier.sendSMS(numberTo, smsBody )
-           self.log("Success")
-            
-        except TwilioRestException as e:
-            self.log("Unsuccessful")
+        smsBody = "TempBase Alert: elevated temperature of " + str(temperature) + " detected."
+        self.sendSMS(recipient, smsBody)
           
-        
     def smsRefill(self, recipient):
-        numberTo = recipient  
-        numberFrom = config["twilio"]["numberFrom"]
-        #numberTo = config["twilio"]["numberTo"]
-        notifier = SMSNotify(numberFrom, config["twilio"]["accountSID"], config["twilio"]["authTok"])
-        
-        try:
-           notifier.sendSMS(numberTo, "TempBase Alert: Sanitiser is almost empty. Please refill")
-           self.log("Success")
-            
-        except TwilioRestException as e:
-            self.log("Unsuccessful")
+        self.sendSMS(recipient, "TempBase Alert: Sanitiser is almost empty. Please refill")
     
-    def smsCapacity(self, recipient):
-        numberTo = recipient  
-        numberFrom = config["twilio"]["numberFrom"]
-        capacity = config["business"]["maxCapacity"]
-        #numberTo = config["twilio"]["numberTo"]
-        notifier = SMSNotify(numberFrom, config["twilio"]["accountSID"], config["twilio"]["authTok"])
-        
-        smsBody = "TempBase Alert: maximum capacity " + str(capacity) + "may be reached."
-
-        try:
-           notifier.sendSMS(numberTo, smsBody)
-           self.log("Success")
-            
-        except TwilioRestException as e:
-            self.log("Unsuccessful")     
+    def smsCapacity(self, recipient, capacity):
+        smsBody = "TempBase Alert: maximum capacity " + str(capacity) + " may be reached."
+        self.sendSMS(recipient, smsBody)
         
--- src/test_sms.py
+++ src/test_sms.py
@@ -1,25 +1,7 @@
 from notifications.notify_sms import SMSNotify
-from twilio.rest import Client
-from twilio.base.exceptions import TwilioRestException
 from config import config
 
-class testSMSNotifications:
-    
-    def testSMS(self):
-        
-        numberTo = "+61411166441"
-        numberFrom = "+61488882708"
-        notifier = SMSNotify(numberFrom, config["twilio"]["accountSID"], config["twilio"]["authTok"])
-        
-        try:
-        # This could potentially throw an exception!
-           notifier.sendSMS(numberTo, "This is a test message")
-           self.log("Success")
-            
-        except TwilioRestException as e:
-            self.log("Unsuccessful")
-            
- 
-if __name__ == "__main__":
-        test = testSMSNotifications()
-        test.testSMS()   
+notifier = SMSNotify(config["twilio"]["systemNumber"], config["twilio"]["accountSID"], config["twilio"]["authTok"])
+for recipient in config["twilio"]["numberTo"]:
+    notifier.smsTemp(recipient, 38.1)
+    notifier.smsRefill(recipient)
