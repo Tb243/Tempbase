143407060ac0c770423ac78cc544ee1c80112678
Richard Denton <isdampe@gmail.com>
2021-09-24 17:32:25 +1000

    FIX: Only open browser on virtual mode.

--- src/fsm/state_setup.py
+++ src/fsm/state_setup.py
@@ -4,6 +4,8 @@ from web.wss_server import WSServer
 from config import config
 import os
 
+VIRTUAL_MODE = True if os.environ.get("virtualMode") == "on" else False
+
 class StateSetup(FsmState):
 
 	def __init__(self, fsm):
@@ -13,7 +15,8 @@ class StateSetup(FsmState):
 		self.httpServer = HttpServer("0.0.0.0", 5000, os.path.dirname(os.path.realpath(__file__)) + "/../web/front/build")
 		self.wsServer = WSServer("0.0.0.0", 8080)
 		self.fsm.addStateHook(self.onStateChange)
-		self.httpServer.openBrowser()
+		if VIRTUAL_MODE:
+			self.httpServer.openBrowser()
 
 	def onStateChange(self, state, stateData):
 		self.wsServer.broadcast({
